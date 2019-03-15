2649ab13186ad2aa6fdb0e8af6c847454799b735
Kamilla <kamilla.1201@gmail.com>
2019-03-15 16:45:43 -0700

    Fixed errors in products api

--- src/routes/products.js
+++ src/routes/products.js
@@ -23,25 +23,25 @@ productsRouter.use('/:id', function (req, res, next) {
   const id = req.params.id
   mongo.getById('products', [id], null, (err, result) => {
     if (err) {
-      return res.status(500).json({ message: 'Failed to complete request' })
+      return res.status(500).json({ error: 'Failed to complete request' })
     }
     console.log(result)
     if (result != null && result.length !== 0) {
       if (result.length > 1) {
-        return res.status(500).json({ message: 'Something went wrong.' })
+        return res.status(500).json({ error: 'Something went wrong.' })
       }
       var product = result[0]
       var ingredients = product.ingredients
       mongo.getById('ingredients', ingredients, null, (err, ingResult) => {
         if (err) {
-          return res.status(500).json({ message: 'Something went wrong.' })
+          return res.status(500).json({ error: 'Something went wrong.' })
         }
         console.log(ingResult)
         product.ingredients = ingResult
         return res.status(200).json(product)
       })
     } else {
-      return res.status(404).json({ message: 'No matched data' })
+      return res.status(404).json({ error: 'No matched data' })
     }
   })
 })
@@ -53,12 +53,12 @@ productsRouter.get('/', (req, res) => {
   let sortCriteria = { posted_date: -1 }
   mongo.get('products', { name: { $regex: new RegExp(RegExp.escape(query), 'i') } }, { name: 1 }, sortCriteria, (err, result) => {
     if (err) {
-      return res.status(500).json({ message: 'Failed to complete request' })
+      return res.status(500).json({ error: 'Failed to complete request' })
     }
     if (result.length !== 0) {
       return res.status(200).json(result)
     }
-    res.status(404).json({ message: 'No matched data' })
+    res.status(404).json({ error: 'No matched data' })
   })
 })
 
